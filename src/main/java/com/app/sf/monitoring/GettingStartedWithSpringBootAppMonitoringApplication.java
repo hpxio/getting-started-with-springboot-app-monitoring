e531a4491a99a3e188bdd24df199d0a411d8053e
HPx <harrsh.hp@live.in>
2020-07-17 14:20:30 +0530

    App-Log::Init-Project

---
+++ src/main/java/com/app/sf/monitoring/GettingStartedWithSpringBootAppMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.app.sf.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GettingStartedWithSpringBootAppMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GettingStartedWithSpringBootAppMonitoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/sf/monitoring/GettingStartedWithSpringBootAppMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.sf.monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GettingStartedWithSpringBootAppMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
